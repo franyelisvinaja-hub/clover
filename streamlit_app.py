f67595ba4b804fe3cc2c905de1729fb0e3171330
mileidy1211 <mileidyblanco1211@gmail.com>
2026-04-05 21:46:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.graph_objects as go
 import plotly.express as px
-
 import streamlit as st
 
 # 1. Configuración de la página (DEBE SER LA PRIMERA LÍNEA DE STREAMLIT)
